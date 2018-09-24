f9912701234781428eef60364202af48981bdc63
rubensrocha <rubens.dragonforce@gmail.com>
2018-09-24 11:07:48 -0300

    API 1.1

--- src/Coinpayments.php
+++ src/Coinpayments.php
@@ -1,7 +1,7 @@
 <?php
 /*
-	CoinPayments.net API Class - v1.0
-	Copyright 2014-2016 CoinPayments.net. All rights reserved.
+	CoinPayments.net API Class - v1.1
+	Copyright 2014-2018 CoinPayments.net. All rights reserved.
 	License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
@@ -39,18 +39,18 @@ class CoinPaymentsAPI {
      * @param amount The amount of the transaction (floating point to 8 decimals).
      * @param currency1 The source currency (ie. USD), this is used to calculate the exchange rate for you.
      * @param currency2 The cryptocurrency of the transaction. currency1 and currency2 can be the same if you don't want any exchange rate conversion.
+     * @param buyer_email Set the buyer's email so they can automatically claim refunds if there is an issue with their payment.
      * @param address Optionally set the payout address of the transaction. If address is empty then it will follow your payout settings for that coin.
      * @param ipn_url Optionally set an IPN handler to receive notices about this transaction. If ipn_url is empty then it will use the default IPN URL in your account.
-     * @param buyer_email Optionally (recommended) set the buyer's email so they can automatically claim refunds if there is an issue with their payment.
      */
-    public function CreateTransactionSimple($amount, $currency1, $currency2, $address='', $ipn_url='', $buyer_email='') {
+    public function CreateTransactionSimple($amount, $currency1, $currency2, $buyer_email, $address='', $ipn_url='') {
         $req = array(
             'amount' => $amount,
             'currency1' => $currency1,
             'currency2' => $currency2,
+            'buyer_email' => $buyer_email,
             'address' => $address,
             'ipn_url' => $ipn_url,
-            'buyer_email' => $buyer_email,
         );
         return $this->api_call('create_transaction', $req);
     }
