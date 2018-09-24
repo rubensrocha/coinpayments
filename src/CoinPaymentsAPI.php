0d1acd5d929c0803d38dcffba162bad02d4be607
rubensrocha <rubens.dragonforce@gmail.com>
2018-09-24 11:31:32 -0300

    API 1.1

--- src/CoinPaymentsAPI.php
+++ src/CoinPaymentsAPI.php
@@ -4,6 +4,7 @@
 	Copyright 2014-2018 CoinPayments.net. All rights reserved.
 	License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.txt
 */
+namespace Rubensrocha\SolveMediaCaptcha;
 
 class CoinPaymentsAPI {
     private $private_key = '';
